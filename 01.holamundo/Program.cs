f2225eecb93782838b0c859bded27a86cc7ca5ef
BeemolAlcala <lfalcalaramirez@gmail.com>
2019-08-20 13:02:41 -0500

    primer commit

---
+++ 01.holamundo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Carlos Castañeda");
+        
+        }
+
+
+            
+    }
+}
