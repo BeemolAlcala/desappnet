8593e23edecfb7d7215c4bd914d906db0c07de12
BeemolAlcala <lfalcalaramirez@gmail.com>
2019-08-22 12:52:16 -0500

    tercer commit

---
+++ 03.areatringulo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03.areatringulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float area, b, a;
+            Console.WriteLine("Program que calcula el area de un triangulo");
+
+            Console.WriteLine(args.Length);
+            if(args.Length<2){
+                Console.WriteLine("Forma de uso \n");
+                Console.WriteLine("Debes pasar los parametros <base> y <altura>");
+                Environment.Exit(0);
+            }
+
+            b = float.Parse(args[0]);
+            a = float.Parse(args[1]);
+            area = b*a/2;
+            Console.WriteLine($"El are es {area}");
+
+        }
+    }
+}
